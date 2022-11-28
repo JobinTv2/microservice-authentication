7dd025af778e0f1e725c58d6fffa908e697e893a
Jobin T v <jobintv@focaloid.com>
2022-11-28 17:12:03 +0530

    feat: Auth service, module and strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
-
+import { PassportModule } from '@nestjs/passport/dist/passport.module';
+import { LocalStrategy } from './strategy/local.startegy';
 @Module({
-  providers: [AuthService],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findOneByEmail(email);
+    if (user && user.password === password) {
+      return user;
+    } else {
+      return null;
+    }
+  }
+}
